1ada312d8705dea8569db2b20555c22a86b94b92
e.ghorbani <Erfan.Ghorbani@snapp.supply>
2022-01-18 14:49:10 +0330

    PostController finished

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,49 +7,43 @@ use App\Models\Post;
 use Facade\FlareClient\Http\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Illuminate\Support\Facades\File;
+use DateTime;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function index()
     {
         $post = Post::all();
         return response()->json($post, HttpFoundationResponse::HTTP_OK);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create(Request $request)
     {
         //
+        $dt = new DateTime();
+
         $upload_image = $request->file('image')->store('uploads/images');
         $upload_thumbnail = $request->file('thumbnail')->store('uploads/thumbnails');
+
         $post = Post::create([
             "title" => $request->title,
             "author_id" => $request->author_id,
             "image" => asset("storage/{$upload_image}"),
             "thumbnail" => asset("storage/{$upload_thumbnail}"),
-            "publish_time" => $request->publish_time,
+            "publish_time" => $dt->format('Y-m-d H:i:s'),
             "body" => $request->body,
         ]);
+
         return "Successfuly added!";
     }
 
 
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
@@ -58,38 +52,42 @@ class PostController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Request $request, $id)
     {
+        $dt = new DateTime();
+
         $post = Post::find($id);
+
+        $upload_image = $request->file('image')->store('uploads/images');
+        $upload_thumbnail = $request->file('thumbnail')->store('uploads/thumbnails');
+
+        File::delete("storage/uploads/images/".basename($post->image));
+        File::delete("storage/uploads/thumbnails/".basename($post->thumbnail));
+
         $post->title = $request->title;
-        //$post->title  = $request->author_id;
-        $post->image = $request->image;
-        $post->thumbnail  = $request->thumbnail;
-        $post->publish_time  = $request->publish_time;
+        $post->image = asset("storage/{$upload_image}");
+        $post->thumbnail  = asset("storage/{$upload_thumbnail}");
+        $post->publish_time  = $dt->format('Y-m-d H:i:s');
         $post->body  = $request->body;
         $post->save();
+
         return "successfuly updated";
     }
 
 
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function delete($id)
     {
         //
         $post = Post::find($id);
+
+        File::delete("storage/uploads/images/".basename($post->image));
+        File::delete("storage/uploads/thumbnails/".basename($post->thumbnail));
+
         $post->delete();
+
         return 'sucess';
     }
 }
