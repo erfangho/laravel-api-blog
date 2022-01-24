650a12037aaba41d5e10f4f61aab4a17fe9c683b
e.ghorbani <Erfan.Ghorbani@snapp.supply>
2022-01-24 16:31:49 +0330

    Create form request for (AuthRequest)

---
+++ app/Http/Requests/AuthRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+            'email'    => 'required|email',
+            'password' => 'required|string|min:6',
+        ];
+    }
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors(), HttpFoundationResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
