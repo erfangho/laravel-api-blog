085560f0f83c7f8b42719eedf93f44051ae5d306
e.ghorbani <Erfan.Ghorbani@snapp.supply>
2022-02-02 15:49:20 +0330

    form request created for post filters

---
+++ app/Http/Requests/IndexFilterRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexFilterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'from' => 'date',
+            'to' => 'date|after_or_equal:from',
+            'date' => 'date',
+            'author' => 'string'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors(), HttpFoundationResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
