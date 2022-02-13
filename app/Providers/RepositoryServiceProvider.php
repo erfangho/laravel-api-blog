0ce87a075cd58140531908f3919d202ee203f390
e.ghorbani <Erfan.Ghorbani@snapp.supply>
2022-02-13 22:39:34 +0330

    PostRepository binded in RepositoryServiceProvider and added to app.php providers

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Models\Repositories\PostRepositoryInterface;
+use App\Models\Repositories\PostRepository;
+
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(PostRepositoryInterface::class, PostRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
