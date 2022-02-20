7b5f465fff99edc664520b74742657899d85e616
e.ghorbani <Erfan.Ghorbani@snapp.supply>
2022-02-20 16:36:49 +0330

    fix chain

--- app/Helpers/Filter/Author.php
+++ app/Helpers/Filter/Author.php
@@ -7,11 +7,13 @@ use Illuminate\Http\Request;
 
 class Author extends Filter
 {
+    public $final;
     public function handle($posts, Request $request)
     {
         if($request->has('author_id')){
             $posts = $posts->where('author_id', $request->author_id);
         }
+        $this->final = $posts->get();
         $this->next($posts, $request);
     }
 }
--- app/Helpers/Filter/Date.php
+++ app/Helpers/Filter/Date.php
@@ -9,12 +9,14 @@ use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class Date extends Filter
 {
+    public $final;
     public function handle($posts, Request $request)
     {
         if($request->has('date')){
             $datetime = new Carbon($request->date.' 00:00:00');
             $posts = $posts->whereDate('created_at', $datetime);
         }
+        $this->final = $posts->get();
         $this->next($posts, $request);
     }
 }
